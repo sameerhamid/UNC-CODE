55da95c7d181db56bc1eec6516bc8031108287e8
Sameer <sameer@theastrotime.com>
2026-06-21 12:56:14 +0530

    learn convert to utf-8

--- Buffers/app.js
+++ Buffers/app.js
@@ -4,4 +4,9 @@ const memoryContainer = Buffer.alloc(4) // 4 bytes (32 bits)
 
 memoryContainer.write("same");
 
-console.log(memoryContainer);
+// const buff = Buffer.from(['0x48', '0x69', '0x21']);
+const buff = Buffer.from('486921', 'hex');
+
+console.log(buff.toString('utf-8'));
+
+// console.log(memoryContainer);
