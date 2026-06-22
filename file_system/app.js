2385c4e09cb7e853b88c88ac21723afd3994e341
Sameer <sameer@theastrotime.com>
2026-06-22 11:36:36 +0530

    three diff ways of doing same thing

--- file_system/app.js
+++ file_system/app.js
@@ -1,7 +1,28 @@
+const fspromises = require('fs/promises');
+
+// const content = fs.readFileSync("./text.txt");
+
+// console.log(content); // Buffer
+
+// console.log(content.toString('utf-8'));
+
+// Promise Api
+
+(async() => {
+    try {
+        await fspromises.copyFile('file.txt', "coppied-promise.txt");
+    } catch (error) {
+        console.log(error);
+    }
+})()
+
 const fs = require('fs');
+// Callback Api
+fs.copyFile('file.txt', "coppied-callback.txt",  (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
 
-const content = fs.readFileSync("./text.txt");
-
-console.log(content); // Buffer
-
-console.log(content.toString('utf-8'));
+// Synchronous API
+fs.copyFileSync('file.txt', "copied-sync.txt");
