8ed8163a919a1e3aec69229b94cb52e9d106e38c
Sameer <sameer@theastrotime.com>
2026-07-29 17:17:43 +0530

    learn about generator functionc

---
+++ generatorFunctions.js
@@ -0,0 +1,61 @@
+function* gen() {
+    console.log("A");
+    yield;
+
+    console.log("B");
+    yield;
+
+    console.log("C");
+    yield;
+
+    console.log("D");
+}
+
+
+const funCall = gen();
+
+// console.log(funCall.next());
+// console.log(funCall.next());
+// console.log(funCall.next());
+// console.log(funCall.next());
+// console.log(funCall.next());
+
+function* fun2() {
+    const a = yield;
+    const b = yield;
+
+    console.log(a + b);
+}
+
+
+const fun2Call = fun2();
+
+// console.log(fun2Call.next());
+// console.log(fun2Call.next(2));
+// console.log(fun2Call.next(3));
+
+function* fun3() {
+    console.log("calling>>>>");
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const fun3Call = fun3();
+// console.log(fun3Call.next());
+// console.log(fun3Call.next());
+// console.log(fun3Call.next());
+// console.log(fun3Call.next().done);
+
+
+function* fun4() {
+    yield* fun3();
+    yield 4;
+    yield 5;
+    yield 6;
+}
+
+
+for(let val of fun4()) {
+    console.log('value>>>>', val)
+}
