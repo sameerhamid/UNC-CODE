624e6735ef64c83b1dc498d77a305101dedf16f0
Sameer <sameer@theastrotime.com>
2026-08-03 16:03:04 +0530

    improve the ui of chat app

--- ChatApp/client.js
+++ ChatApp/client.js
@@ -6,14 +6,40 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-const socket = net.createConnection({port: 3008, host: '127.0.0.1'}, async () => {
-    console.log("Connected to the server!");
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        })
+    })
+}
+
+const moveCursor = (dx, dy) => {
+    return new Promise((resolve, reject) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve();
+        })
+    })
+}
+
+const ask = async () => {
     const message = await rl.question('Enter a message > ');
     socket.write(message);
+}
+
+const socket = net.createConnection({port: 3008, host: '127.0.0.1'}, async () => {
+    console.log("Connected to the server!");
+    await moveCursor(0, -1) // move the cursor one line up
+    await clearLine(0); // clear the current line that the cursor is in
+    ask();
 });
 
-socket.on('data', (data) => {
+socket.on('data', async (data) => {
+    console.log("");
+    await moveCursor(0, -1) // move the cursor one line up
+    await clearLine(0); // clear the current line that the cursor is in
     console.log(data.toString('utf-8'));
+    ask();
 })
 
 socket.on('end', () => {
