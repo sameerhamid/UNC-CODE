e9c48a5f3f738b3c716580c4c18ac187a395b380
Sameer <sameer@theastrotime.com>
2026-08-03 16:48:24 +0530

    idenetify users

--- ChatApp/client.js
+++ ChatApp/client.js
@@ -6,6 +6,8 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+let id = '';
+
 const clearLine = (dir) => {
     return new Promise((resolve, reject) => {
         process.stdout.clearLine(dir, () => {
@@ -24,24 +26,35 @@ const moveCursor = (dx, dy) => {
 
 const ask = async () => {
     const message = await rl.question('Enter a message > ');
-    socket.write(message);
+    await moveCursor(0, -1);
+    await clearLine(0);
+    socket.write(`${id}-message-${message}`);
+    ask();                     // loop here, not from the data handler
 }
 
-const socket = net.createConnection({port: 3008, host: '127.0.0.1'}, async () => {
+
+const socket = net.createConnection({ port: 3008, host: '127.0.0.1' }, async () => {
     console.log("Connected to the server!");
-    await moveCursor(0, -1) // move the cursor one line up
-    await clearLine(0); // clear the current line that the cursor is in
     ask();
 });
 
 socket.on('data', async (data) => {
+    // when we are getting a message
     console.log("");
     await moveCursor(0, -1) // move the cursor one line up
     await clearLine(0); // clear the current line that the cursor is in
-    console.log(data.toString('utf-8'));
-    ask();
+    // when we are getting the id
+    if (data.toString('utf-8').startsWith('id-')) {
+        // everything from the 3rd char up until the end
+        id = data.toString('utf-8').substring(3);
+        console.log(`Your id is ${id}!\n`);
+    } else {
+        console.log(data.toString('utf-8'));
+    }
+    rl.prompt(true);
 })
 
 socket.on('end', () => {
+    rl.close();
     console.log("Connection was ended!");
 })
