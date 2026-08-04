6076a645c80d913af0179e97b056e60709333bd4
Sameer <sameer@theastrotime.com>
2026-08-04 11:57:17 +0530

    notifying everyone when a client joins or leaves

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -8,6 +8,10 @@ const clients = [];
 server.on('connection', (socket) => {
     console.log("A new connection to the server!");
     const clientId = clients.length + 1;
+    // Broadcasting everyone when someone enters the chat room.
+    clients.forEach(client => {
+        client.socket.write(`User ${clientId} joined! `);
+    })
     socket.write(`id-${clientId}`);
     socket.on('data', (data) => {
         const dataString = data.toString('utf-8');
@@ -15,6 +19,13 @@ server.on('connection', (socket) => {
         const message = dataString.substring(dataString.indexOf('-message-') + 9);
         clients.forEach(client => {
             client.socket.write(`User ${id}: ${message}`);
+        })
+    })
+
+    // Broadcasting everyone when someone leaves the chat room.
+    socket.on('end', () => {
+        clients.forEach(client => {
+            client.socket.write(`User ${clientId} left!`);
         })
     })
     clients.push({id: clientId.toString(), socket});
